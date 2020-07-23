af9ca8ab1491840d52439fa8f95250d71f9c84e0
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-23 15:52:53 +0530

    folders and user model created (crypto password)

---
+++ backend/models/user.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose")
+const crypto = require("crypto")
+const uuid = require("uuid/v1")
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  userinfo: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  encryptedpassword: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  salt: {
+    type: String,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+})
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password
+    this.salt = uuid()
+    this.encryptedpassword = this.securePassword(password)
+  })
+  .get(function () {
+    return this._password
+  })
+
+userSchema.method = {
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encryptedpassword
+  },
+
+  securePassword: function (plainPassword) {
+    if (!password) return ""
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex")
+    } catch (err) {
+      return ""
+    }
+  },
+}
+
+module.exports = mongoose.model("User", userSchema)
