d53e6c96784f10e3994607ecfcc51795588c091f
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-23 16:25:41 +0530

    products schema added

---
+++ backend/models/product.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 2000,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    totalstock: {
+      type: Number,
+      required: true,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    image: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("Product", productSchema)
