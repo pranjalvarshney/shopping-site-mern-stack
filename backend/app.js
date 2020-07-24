497f5428f2739265dc6b6cd8f215d05011b203a7
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-24 06:51:38 +0530

    server and mongoose connected

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,17 @@
-const express = require("express");
+const express = require("express")
+const app = express()
+
+const mongoose = require("mongoose")
+
+const uri = `mongodb://localhost:27017/test`
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database connected")
+  })
+  .catch((err) => console.log(err))
+
+const port = 4000
+app.listen(port, () => {
+  console.log(`Server stated at PORT: ${port} successfully`)
+})
