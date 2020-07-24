8db8e8fbc34a5f23058c33a3f0231b2cd91709f7
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-24 07:38:41 +0530

    env variable added

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express()
 
 const mongoose = require("mongoose")
 
-const uri = `mongodb://localhost:27017/test`
+const uri = process.env.MONGO_URI
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -11,7 +11,7 @@ mongoose
   })
   .catch((err) => console.log(err))
 
-const port = 4000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
   console.log(`Server stated at PORT: ${port} successfully`)
 })
