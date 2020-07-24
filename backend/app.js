f89d377a29e42a9ebb4322d412dffb20d8621230
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-24 08:18:54 +0530

    middlewares added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const app = express()
-
 const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+//middlewares
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+// mongodb connection
 
 const uri = process.env.MONGO_URI
 mongoose
@@ -11,6 +18,7 @@ mongoose
   })
   .catch((err) => console.log(err))
 
+// listen to server
 const port = process.env.PORT || 4000
 app.listen(port, () => {
   console.log(`Server stated at PORT: ${port} successfully`)
