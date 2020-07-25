bf18a02b525f10c84988e0e0a698c44bbc6b690d
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-25 12:32:18 +0530

    signin route added with token added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,13 @@
 const User = require("../models/user")
 const { validationResult } = require("express-validator")
+const jwt = require("jsonwebtoken")
+const expressJWT = require("express-jwt")
 
 exports.signup = (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(422).json({
-      error: errors.array()[0].msg,
+      errormsg: errors.array()[0].msg,
     })
   }
   const { firstname, lastname, email, password } = req.body
@@ -13,11 +15,55 @@ exports.signup = (req, res) => {
   newUser.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        msg: "An error occured while processing the request",
+        errormsg: "An error occured while processing the request",
       })
     }
     return res.status(200).json({
       data: user,
+    })
+  })
+}
+
+exports.signin = (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      errormsg: errors.array()[0].msg,
+    })
+  }
+
+  const { email, password } = req.body
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      return res.status(400).json({
+        errormsg: "an error occured",
+      })
+    }
+    if (!user) {
+      return res.status(400).json({
+        errormsg: "email not found",
+      })
+    }
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        errormsg: "Wrong credentials",
+      })
+    }
+    //creating token
+    const token = jwt.sign({ id: user._id }, process.env.SECRET)
+    //put token in cookie
+    res.cookie("token", token, { expire: new Date() + 9999 })
+    //send response to frontend
+    const { _id, firstname, lastname, email, role } = user
+    res.status(200).json({
+      token,
+      data: {
+        _id,
+        firstname,
+        lastname,
+        email,
+        role,
+      },
     })
   })
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { signup } = require("../controllers/auth")
+const { signup, signin } = require("../controllers/auth")
 const { check } = require("express-validator")
 
 router.post(
@@ -18,6 +18,17 @@ router.post(
       .withMessage("password should be greater than 6 characters"),
   ],
   signup
+)
+
+router.post(
+  "/signin",
+  [
+    check("email").isEmail().withMessage("enter a valid email"),
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("password should be greater than 6 characters"),
+  ],
+  signin
 )
 
 module.exports = router
