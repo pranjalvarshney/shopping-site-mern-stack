fade9c50c71ca9d9af62730e14cdb75322c48364
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-08-04 15:40:15 +0530

    react-router-dom setup complete and home page added

---
+++ frontend/src/Routes.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Home } from "./core/Home"
+
+export const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Router>
+  )
+}
