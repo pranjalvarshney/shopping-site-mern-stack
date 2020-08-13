1330b06a5a8a0129cbccf48aed79f0e1e626cf0b
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-08-13 15:07:31 +0530

    admin and private routes added

---
+++ frontend/src/auth/PrivateRoutes.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { isAuthenticated } from "./helper"
+
+export const PrivateRoutes = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/signin", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  )
+}
