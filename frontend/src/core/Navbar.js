4ce5c813d95b24139f3c045f1e06e1ff6ea7cb5e
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-08-13 14:29:16 +0530

    conditional routes

--- frontend/src/core/Navbar.js
+++ frontend/src/core/Navbar.js
@@ -1,5 +1,6 @@
-import React from "react"
-import { Link, withRouter } from "react-router-dom"
+import React, { Fragment } from "react"
+import { Link, withRouter, Redirect } from "react-router-dom"
+import { isAuthenticated, signout } from "../auth/helper"
 
 const currentTab = (history, path) => {
   if (history.location.pathname === path) {
@@ -39,24 +40,7 @@ const Navbar = ({ history }) => {
                 Home
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link
-                style={currentTab(history, "/signup")}
-                className='nav-link'
-                to='/signup'
-              >
-                Signup
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                style={currentTab(history, "/signin")}
-                className='nav-link'
-                to='/signin'
-              >
-                Signin
-              </Link>
-            </li>
+
             <li className='nav-item'>
               <Link
                 style={currentTab(history, "/cart")}
@@ -75,6 +59,42 @@ const Navbar = ({ history }) => {
                 Account
               </Link>
             </li>
+            {!isAuthenticated() && (
+              <Fragment>
+                <li className='nav-item'>
+                  <Link
+                    style={currentTab(history, "/signup")}
+                    className='nav-link'
+                    to='/signup'
+                  >
+                    Signup
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    style={currentTab(history, "/signin")}
+                    className='nav-link'
+                    to='/signin'
+                  >
+                    Signin
+                  </Link>
+                </li>
+              </Fragment>
+            )}
+            {isAuthenticated() && (
+              <li className='nav-item'>
+                <span
+                  className='nav-link'
+                  onClick={() => {
+                    signout(() => {
+                      history.push("/")
+                    })
+                  }}
+                >
+                  Signout
+                </span>
+              </li>
+            )}
           </ul>
         </div>
       </nav>
