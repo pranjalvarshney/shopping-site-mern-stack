2861be006c43fb5d3eeacb9bf49e928a5f3bd8e9
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-08-22 23:23:44 +0530

    blog model added

---
+++ backend/models/blog.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    max: 50,
+  },
+  tagline: {
+    type: String,
+    max: 100,
+    trim: true,
+  },
+  description: {
+    type: String,
+    max: 3000,
+  },
+  picture: {
+    data: Buffer,
+    contentType: String,
+  },
+  like: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+})
+
+module.exports = mongoose.model("Blog", blogSchema)
