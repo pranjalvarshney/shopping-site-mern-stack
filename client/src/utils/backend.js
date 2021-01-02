7ebf36caf0e573ac2895bb25b9cd6f089c90a4c9
Pranjal Varshney <pranjal25varshney@gmail.com>
2021-01-02 18:44:26 +0530

    version 2

--- client/src/utils/backend.js
+++ client/src/utils/backend.js
@@ -1,1 +1,1 @@
-export const API = "http://localhost:4000/api"
+export const API = process.env.REACT_APP_BACKEND
--- controllers/makePayment.js
+++ controllers/makePayment.js
@@ -28,16 +28,16 @@ exports.stripePayment = (req, res) => {
             customer: customer.id,
             receipt_email: token.email,
             description: "Test data ok!",
-            // shipping: {
-            //   name: token.card.name,
-            //   address: {
-            //     line1: token.card.address_line1,
-            //     line2: token.card.address_line2,
-            //     city: token.card.address_city,
-            //     country: token.card.address_country,
-            //     postal_code: token.card.address_zip,
-            //   },
-            // },
+            shipping: {
+              name: token.card.name,
+              address: {
+                line1: token.card.address_line1,
+                line2: token.card.address_line2,
+                city: token.card.address_city,
+                country: token.card.address_country,
+                postal_code: token.card.address_zip,
+              },
+            },
           },
           { idempotencyKey }
         )
